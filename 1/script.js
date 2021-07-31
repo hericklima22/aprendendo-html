4d676d77f66aa36dfd8bdfac5c6553e5fe6c1777
hericklima22 <herick.lima77@gmail.com>
2021-07-31 19:19:25 -0300

    modificando o estilo e os codigos da pagina

--- 1/script.js
+++ 1/script.js
@@ -1,9 +1,13 @@
 window.onload = () => {
     const audios = ['audio1.mp3', 'audio2.mp3', 'audio3.mp3', 'audio4.mp3', 'audio5.mp3', 'audio6.mp3']
     const audioIndex = Math.floor(Math.random() * 6)
+    console.log(Math.floor(Math.random() * 6))
     const audioToPlay = audios[audioIndex]
-    const audioPath = `/audios/${audioToPlay}`
+    const audioPath = "file:///C:/Users/heric/Documents/aprendendo-html/1/${audioToPlay}"
+
+   
 
     const source = document.getElementById('autoPlay')
-    source.setAttribute('src', audioPath)
+    source.setAttribute("src", audioPath)
+    source.setAttribute("type", "audio/mpeg")
 }
