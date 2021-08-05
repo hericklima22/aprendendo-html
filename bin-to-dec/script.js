7cd43ac1e649a9c18d7534b58734ef2c01adb287
hericklima22 <herick.lima77@gmail.com>
2021-08-05 17:32:08 -0300

    logica implementada, codigo funcionando v1.0

--- bin-to-dec/script.js
+++ bin-to-dec/script.js
@@ -1,7 +1,38 @@
 function traduzir() {
+  let entrada = document.querySelector(".input-text-bin")
+  let binario = entrada.value
+  let binArray = []
+  let html
+
+  /*VERIFICAÇÃO DA ENTRADA*/
+
+  for (let bin in binario) {  // "transforma a string em array"
+    binArray.push(binario[bin])
+  }
+
+  if(binArray.some((value) => { //verifica se tem algum valor diferente de 0 ou 1 no array
+    return value !== '0' && value !== '1' //retorna se tem ou nao
+  })) {
+    alert("A entrada não é um número binário")  //se tiver, cria uma alerta e finaliza a funçao
+    return
+  }
+
+  /* LóGICA DE TRANSFORMAÇÃO DE BINARIO PARA DECIMAL */
+  let decimal = 0
+  let index = binArray.length - 1
+
+  for(let num in binArray) {
+    if(binArray[num] === '1') {
+      decimal += 2 ** index
+    }
+    index--
+  }
+
+  let saida = document.querySelector(".output-text-dec")
+  saida.value = decimal
 
 }
 
-function apagar {
-    
+function apagar() {
+
 }
