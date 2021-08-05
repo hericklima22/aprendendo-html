302b21d13d8800c7bb437cf276ee6055147e47bd
hericklima22 <herick.lima77@gmail.com>
2021-08-05 19:56:41 -0300

    refactor: melhorando a logica de traduçao com o carlao

--- bin-to-dec/script.js
+++ bin-to-dec/script.js
@@ -2,40 +2,26 @@ function traduzir() {
   let entrada = document.querySelector(".input-text-bin")
   let binario = entrada.value
   let binArray = []
-  let html
 
   /*VERIFICAÇÃO DA ENTRADA*/
 
-  for (let bin in binario) {  // "transforma a string em array"
+  for (let bin in binario) {  //transforma a string em array
     binArray.push(binario[bin])
   }
 
-  if(binArray.some((value) => { //verifica se tem algum valor diferente de 0 ou 1 no array
-    return value !== '0' && value !== '1' //retorna se tem ou nao
-  })) {
+  if(binArray.some((value) => value !== '0' && value !== '1')) { //verifica se tem algum valor diferente de 0 ou 1 no array
     alert("A entrada não é um número binário")  //se tiver, cria uma alerta e finaliza a funçao
     return
   }
 
-  /* LóGICA DE TRANSFORMAÇÃO DE BINARIO PARA DECIMAL */
+  /* LÓGICA DE TRANSFORMAÇÃO DE BINARIO PARA DECIMAL */
   let decimal = 0
-  let index = binArray.length - 1
+  binArray.filter((value) => value == '1').forEach((element, index) => decimal += (2 ** (binArray.length - (index + 1))))
 
-  for(let num in binArray) {
-    if(binArray[num] === '1') {
-      decimal += 2 ** index
-    }
-    index--
-  }
-
-  let saida = document.querySelector(".output-text-dec")
-  saida.value = decimal
-
+  document.querySelector(".output-text-dec").value = decimal
 }
 
 function apagar() {
-  let entrada = document.querySelector(".input-text-bin")
-  let saida = document.querySelector(".output-text-dec")
-  entrada.value = ''
-  saida.value = ''
+  document.querySelector(".input-text-bin").value = ''
+  document.querySelector(".output-text-dec").value = ''
 }
