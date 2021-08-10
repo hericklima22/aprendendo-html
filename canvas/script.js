8895d61be7a6b87485b5364692eb37cbefe32727
hericklima22 <herick.lima77@gmail.com>
2021-08-09 21:13:15 -0300

    tentando implementar a logica

--- canvas/script.js
+++ canvas/script.js
@@ -1,12 +1,27 @@
-var canvas = document.getElementById('#container').getElementById('.canvas')
+var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
+var squareSize = 20
+
+var canvasRowInitial = 0,
+    canvasColumnInitial = 0,
+    canvasRowFinal = canvas.width - squareSize,
+    canvasColumnFinal = canvas.height - squareSize,
+    wallUp = 0, wallDown = 0, wallRight = 0, wallLeft = 0,
+    speed = 5, row = 0, col = 0
 
 function desenhar() {
-    for (var i = 0; i < 6; i++){
-        for (var j = 0; j < 6; j++){
-            ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ',' +
-                            Math.floor(255 - 42.5 * j) + ',0)';
-            ctx.fillRect(j * 25, i * 25, 25, 25);
-        }
+    requestAnimationFrame(desenhar)
+    ctx.fillStyle = 'white'
+
+    
+    if(col < canvasColumnFinal - wallRight) {
+        ctx.fillRect(col += speed , row, squareSize, squareSize)
+        if(col == canvasColumnFinal - wallRight) {wallRight += squareSize; console.log(wallRight)}
+    } else if(row < canvasRowFinal - wallDown) {
+        ctx.fillRect(col, row += speed, squareSize, squareSize)
+        
+    } else if (col > 0) {
+        ctx.fillRect(col -= speed, row, squareSize, squareSize)
     }
+    
 }
