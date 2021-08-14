1913a6475400ead460eb91a038451f2325237528
hericklima22 <herick.lima77@gmail.com>
2021-08-14 19:56:33 -0300

    tabela pronta

--- table-draw/script.js
+++ table-draw/script.js
@@ -1,21 +1,31 @@
 const column = 10
 const row = 10
 
-function drawMatrix(column, row) {
+const array = []
+
+function start() {
+
+    drawMatrix(column, row)
+}
+
+function drawMatrix(column1, row1) {
     let html = '<table cellpadding=0 cellspacing=0>'
 
-    for(let i = 0; i < row; i++) {
+    for(let row = 0; row < row1; row++) {
         html += '<tr>'
-        for (let j = 0; j < column; j++) {
-            html += '<td>'
-            html += `<div class="cell-index">${j}</div>`
-            html += '</td>'
-        }
-        html += '</tr>'
+            
+            for(let column = 0; column < column1; column++) {
+                html += '<td>'
+                html += `<div class="pixel-index">${column}</div>`
+                html += '</td>'
+            }
+
+            html += '</tr>'
     }
 
-    html = '</table>'
+    html += '</table>'
+
     document.querySelector('#matrix').innerHTML = html
 }
 
-drawMatrix(column, row)
+start()
